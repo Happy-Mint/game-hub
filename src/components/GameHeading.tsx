845e80933cea54c4ea7387ba40c5b69d5ecc5518
cryptocc <2270857759@qq.com>
2023-09-10 17:13:07 +0800

    Add a dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,24 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+const GameHeading = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name || ""}  ${
+    gameQuery.genre?.name || ""
+  } Games`;
+  return (
+    //Games
+    //Action Games
+    //Xbox Game
+    // Xbox Action Games
+
+    <Heading as="h1" marginY={5} fontSize={"5xl"}>
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
