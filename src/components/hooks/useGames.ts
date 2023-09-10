13c1b9a2c60048e5c05c7d2cb32c15f53791350d
cryptocc <2270857759@qq.com>
2023-09-10 17:46:05 +0800

    Add emojis

--- src/components/hooks/useGames.ts
+++ src/components/hooks/useGames.ts
@@ -6,9 +6,10 @@ import { Platform } from './usePlatForms';
 export interface Game {
     id: number;
     name: string;
-    background_image:string
-    parent_platforms : {platform : Platform}[]
-    metacritic : number
+    background_image:string;
+    parent_platforms : {platform : Platform}[];
+    metacritic : number;
+    rating_top : number ;
   }
 
   
